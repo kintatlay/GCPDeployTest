f96f7a8ff6a14356ffb35f0e4a6a39a24875a1fc
Kin Lay <hongkongbboy@gmail.com>
2020-08-09 05:59:47 -0700

    test

---
+++ frontend/build/precache-manifest.a158b8c9917e7b32eb0b69f474404ec8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "567bafb26c46832edfc7f627c816216b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a7f39e336e803a80715",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6278127a4acb8c72ddb3",
+    "url": "/static/js/2.4ab78bef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ab78bef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a7f39e336e803a80715",
+    "url": "/static/js/main.8642ea52.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
